e21904e0f7185593b7e3d02ae4357427d086b042
91RedBull21 <baltazarpatane12@gmail.com>
2024-12-10 16:41:25 -0300

    Update Controlador (Balta).java

--- src/Aplicacion/Controlador.java
+++ src/Aplicacion/Controlador.java
@@ -1,32 +1,55 @@
-package Aplicacion;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Controlador {
-	private Modelo modelo;
+	
 	private Vista vista;
+	private Modelo modelo;
 	
 	public Controlador(Vista vista, Modelo modelo) {
 		this.vista = vista;
 		this.modelo = modelo;
 		
-		/*tengo que asignar todos los listener a cada boton de toda la aplicacion que haya, para eso creo una clase por cada pantallaso, entonces cada pantallaso tiene como atributos todos los botones y todo,
-		 *  entonces cuando asigno la vista ya tengo todos los botones para acceder con punto*/
-		
-		
-		
-		
-		
+		vista.getPanelPrincipal().getRegistrarseButton().addActionListener(new BotonRegistrarsePanelPrincipal());
+		vista.getPanelRegistro().getCierreButton().addActionListener(new BotonCerrarPanelRegistro());
+		vista.getPanelPrincipal().getLoginButton().addActionListener(new BotonIniciarSesionPanelPrincipal());
+		vista.getPanelRegistro().getRegistrarButton().addActionListener(new BotonRegistrarPanelRegistro());
 		
 	}
 	
-	public class BotonVentanaRegistrarListener implements ActionListener{
+	public class BotonRegistrarsePanelPrincipal implements ActionListener{
 
 		public void actionPerformed(ActionEvent e) {
-			/*tengo que hacer que no se vea mas la pantalla de login y que se pase a ver la pantalla para registrarse*/
+			vista.mostrarPanel(vista.getPanelRegistro());
+			vista.repaint();
 		}
 		
 	}
+	
+	public class BotonCerrarPanelRegistro implements ActionListener{
 
+		public void actionPerformed(ActionEvent e) {
+			vista.mostrarPanel(vista.getPanelPrincipal());
+			vista.repaint();
+		}
+		
+	}
+	
+	public class BotonIniciarSesionPanelPrincipal implements ActionListener{
+
+		public void actionPerformed(ActionEvent e) {
+			vista.mostrarPanel(vista.getPanelActivos());
+			vista.repaint();
+		}
+		
+	}
+	
+	public class BotonRegistrarPanelRegistro implements ActionListener{
+
+		public void actionPerformed(ActionEvent e) {
+			vista.mostrarPanel(vista.getPanelActivos());
+			vista.repaint();
+		}
+		
+	}
 }
