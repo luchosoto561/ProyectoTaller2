0aa9a663696c9274a6914f5702d248dbd6d60361
91RedBull21 <baltazarpatane12@gmail.com>
2024-12-10 23:48:41 -0300

    Update Controlador (Balta 10/12).java

--- src/Aplicacion/Controlador.java
+++ src/Aplicacion/Controlador.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionListener;
 public class Controlador {
 	
 	private Vista vista;
+	@SuppressWarnings("unused")
 	private Modelo modelo;
 	
 	public Controlador(Vista vista, Modelo modelo) {
@@ -11,45 +12,89 @@ public class Controlador {
 		this.modelo = modelo;
 		
 		vista.getPanelPrincipal().getRegistrarseButton().addActionListener(new BotonRegistrarsePanelPrincipal());
-		vista.getPanelRegistro().getCierreButton().addActionListener(new BotonCerrarPanelRegistro());
 		vista.getPanelPrincipal().getLoginButton().addActionListener(new BotonIniciarSesionPanelPrincipal());
-		vista.getPanelRegistro().getRegistrarButton().addActionListener(new BotonRegistrarPanelRegistro());
+		vista.getPanelPrincipal().getCierreButton().addActionListener(new BotonCerrarPanelPrincipal());
 		
+		vista.getPanelRegistro().getRegistrarButton().addActionListener(new BotonRegistrarPanelRegistro());
+		vista.getPanelRegistro().getCierreButton().addActionListener(new BotonCerrarPanelRegistro());
+		
+		vista.getPanelActivos().getBtnCerrarSesion().addActionListener(new BotonCerrarPanelMenu());
+		vista.getPanelActivos().getBtnCotizaciones().addActionListener(new BotonCotizacionesMisActivos());
+		vista.getPanelActivos().getBtnMisOperaciones().addActionListener(new BotonOperacionesMisActivos());
+		
+		vista.getPanelCotizaciones().getBtnVolver().addActionListener(new BotonVolverCotizaciones());
+		
+		vista.getPanelMisOperaciones().getBtnVolver().addActionListener(new BotonVolverOperaciones());
 	}
 	
 	public class BotonRegistrarsePanelPrincipal implements ActionListener{
-
 		public void actionPerformed(ActionEvent e) {
 			vista.mostrarPanel(vista.getPanelRegistro());
 			vista.repaint();
 		}
-		
+	}
+	
+	public class BotonIniciarSesionPanelPrincipal implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			vista.getPanelActivos().nombreUsuario(vista.getPanelPrincipal().getGmail().getText());
+			vista.mostrarPanel(vista.getPanelActivos());
+			vista.repaint();
+		}
+	}
+	
+	public class BotonCerrarPanelPrincipal implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			System.exit(0);
+		}
+	}
+	
+	public class BotonRegistrarPanelRegistro implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			vista.getPanelActivos().nombreUsuario(vista.getPanelRegistro().getEmail().getText());
+			vista.mostrarPanel(vista.getPanelActivos());
+			vista.repaint();
+		}
 	}
 	
 	public class BotonCerrarPanelRegistro implements ActionListener{
-
 		public void actionPerformed(ActionEvent e) {
 			vista.mostrarPanel(vista.getPanelPrincipal());
 			vista.repaint();
 		}
-		
 	}
 	
-	public class BotonIniciarSesionPanelPrincipal implements ActionListener{
-
+	public class BotonCerrarPanelMenu implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			vista.mostrarPanel(vista.getPanelPrincipal());
+			vista.repaint();
+		}
+	}
+	
+	public class BotonCotizacionesMisActivos implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			vista.mostrarPanel(vista.getPanelCotizaciones());
+			vista.repaint();
+		}
+	}
+	
+	public class BotonOperacionesMisActivos implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			vista.mostrarPanel(vista.getPanelMisOperaciones());
+			vista.repaint();
+		}
+	}
+	
+	public class BotonVolverCotizaciones implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
 			vista.mostrarPanel(vista.getPanelActivos());
 			vista.repaint();
 		}
-		
 	}
 	
-	public class BotonRegistrarPanelRegistro implements ActionListener{
-
+	public class BotonVolverOperaciones implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
 			vista.mostrarPanel(vista.getPanelActivos());
 			vista.repaint();
 		}
-		
 	}
 }
