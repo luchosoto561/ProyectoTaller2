f2f953e63ac84de569f97a35a7dfdcc2ba726bc2
91RedBull21 <baltazarpatane12@gmail.com>
2025-02-05 15:41:18 -0300

    Update PanelRegistro.java

--- src/Aplicacion/PanelRegistro.java
+++ src/Aplicacion/PanelRegistro.java
@@ -82,6 +82,7 @@ public class PanelRegistro extends JPanel {
     	add(password);
     	
     	// Configuración del checkbox para aceptar términos
+        aceptarTerminos = new JCheckBox("Aceptar términos y condiciones");
     	aceptarTerminos.setBounds(195, 260, 250, 30);
     	aceptarTerminos.setBackground(new Color(222, 184, 135)); // Marroncito
     	add(aceptarTerminos);
